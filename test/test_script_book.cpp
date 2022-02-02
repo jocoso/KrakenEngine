dc606aa53d379c3999b0ed336c6c51c7bff83112
jocoso <joshua.collado022@gmail.com>
2022-02-02 15:03:54 -0500

    yes

--- test/test_script_book.cpp
+++ test/test_script_book.cpp
@@ -3,11 +3,14 @@
 #include <fstream>
 
 #include "../include/script_book.h"
+#include "../include/narrator.h"
 #include "../rapidxml-1.13/rapidxml.hpp"
-
+#include "../include/runner.h"
 
 int main() {
     rawr::script_book book;
+    rawr::narrator narrator;
+    rawr::runner main_loop;
 
     std::ifstream theFile( book.get_current_dir() + "\\scripts\\chapter1.xml" );
     
