560fc8769b51750c6ee95b8cbe07e710af383ef5
jocoso <joshua.collado022@gmail.com>
2022-03-08 13:42:11 -0500

    Prototyping

--- Main.cpp
+++ Main.cpp
@@ -1,43 +1,40 @@
 #include <iostream>
-#include <algorithm>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#include "triangle.h"
-#include "kraken_theater.h"
-
-void printArray(const float* array, int n) {
-	for (int i = 0; i < n; i++) {
-		std::cout << array[i] << std::endl;
-	}
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
 }
 
+int main(void) {
+	glfwInit();
 
-int main() {
-	KrakenTheater kt(800, 800, "Kraken Prototype v.1");
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
-	kt.changeCurtainsColor(0.07f, 0.13f, 0.17f);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	float vertices[] = {
-		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,      // Lower Left
-		0.5, -0.5f * float(sqrt(3)) / 3, 0.0f,        // Lower Right
-		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,    // Upper Corner
-		-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,   // Inner Left    
-		0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,   // Inner right
-		0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f         // Inner down
-	};
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Josh", NULL, NULL);
 
-	unsigned int indices[] = {
-		0, 3, 5,	// Lower left
-		3, 2, 4,	// Lower right
-		5, 4, 1		// Upper triangle
-	};
+	if (window == NULL) {
+		std::cout << "Failed to create a GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
 
-	Triangle t(vertices, indices);
-	Shape* s = &t;
+	glfwMakeContextCurrent(window);
 
-	kt.openCurtains(s);
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return 0;
+	}
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 	
+	while (!glfwWindowShouldClose(window)) {
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
 
 	return 0;
 }
